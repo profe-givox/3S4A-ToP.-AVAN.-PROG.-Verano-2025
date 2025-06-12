6d5c0d8cd53b22d7b327301ca9647acb04d55924
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2025-06-12 12:36:13 -0600

    Se creo el proyecto de biblioteca de clases para empaquetamemiento y publicacion a través de nuget.org

---
+++ AppLogger/AppLogger/Logger.cs
@@ -0,0 +1,13 @@
+﻿namespace AppLogger
+{
+    public class Logger 
+    {
+        public void Log(string message)
+        {
+            // Here you would implement the logic to log the message
+            // For example, writing to a file or console
+            System.Console.WriteLine($"Log: {message}");
+        }
+
+    }
+}
