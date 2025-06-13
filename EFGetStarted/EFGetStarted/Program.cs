23c48344111106e3fd53256903bfde00ac73f5a6
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2025-06-13 13:36:22 -0600

    Se subio el proyecto que usa EF

---
+++ EFGetStarted/EFGetStarted/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+// See https://aka.ms/new-console-template for more information
+using EFGetStarted;
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+
+
+ 
+ using var db = new BloggingContext();
+// Note: This sample requires the database to be created before running.
+Console.WriteLine($"Database path: {db.DbPath}.");
+// Create
+Console.WriteLine("Inserting a new blog");
+db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+await db.SaveChangesAsync();
+
+Console.WriteLine("Hello, World!");
