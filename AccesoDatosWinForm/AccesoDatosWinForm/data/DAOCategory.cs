1d8a854f8cb30ad38c5f2e25d1e35294a86b722f
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2025-06-25 12:16:23 -0600

    Se finalizaron los metodos del DAO

--- AccesoDatosWinForm/AccesoDatosWinForm/data/DAOCategory.cs
+++ AccesoDatosWinForm/AccesoDatosWinForm/data/DAOCategory.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Drawing.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,6 +32,62 @@ namespace AccesoDatosWinForm.data
 
             return resultado;
         }
+
+        public bool delete(int id)
+        {
+            using (_ad = new AccesoDatosMySql("localhost", "nwind",
+                "root", "700r", 3306))
+            {
+                var parametro = new Dictionary<string, object>
+                {
+                    { "@CategoryID", id }
+                };
+                var sentenciaSQL = "DELETE FROM Categories WHERE " +
+                    "CategoryID = @CategoryID;";
+                return _ad.ejecutarSentencia(sentenciaSQL, parametro) > 0;
+            }
+        }
+
+        public bool update(Category obj)
+        {
+            using (_ad = new AccesoDatosMySql("localhost", "nwind",
+                "root", "700r", 3306))
+            {
+                var parametros = new Dictionary<string, object>
+                {
+                    { "@CategoryID", obj.CategoryID },
+                    { "@CategoryName", obj.CategoryName },
+                    { "@Description", obj.Description }
+                };
+                var sentenciaSQL = "UPDATE Categories SET " +
+                    "CategoryName = @CategoryName, " +
+                    "Description = @Description " +
+                    "WHERE CategoryID = @CategoryID;";
+                return _ad.ejecutarSentencia(sentenciaSQL, parametros) > 0;
+            }
+        }
+
+        public Category getOneById(int id)
+        {
+            using (_ad = new AccesoDatosMySql("localhost", "nwind",
+                "root", "700r", 3306))
+            {
+                var parametro = new Dictionary<string, object>
+                {
+                    { "@CategoryID", id }
+                };
+                var sentencia = "Select * from Categories where CategoryID=@CategoryID";
+                _ad.queryTable(sentencia, parametro);
+                var dt = _ad.queryTable(sentencia, parametro);
+
+                return new Category
+                {
+                    CategoryID = (int)dt.Rows[0]["CategoryID"],
+                    CategoryName = dt.Rows[0]["CategoryName"].ToString(),
+                    Description = dt.Rows[0]["Description"].ToString()
+                };
+            }
+            }
 
         public List<Category> getAll() { 
             var lst = new List<Category>();
